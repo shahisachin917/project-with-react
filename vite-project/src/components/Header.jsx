1b23283625930a9150b22b46799880f9c5997575
Sachin Shahi <schn_jachin@hotmail.com>
2022-12-12 20:15:43 -0600

    Applying CSS module

---
+++ vite-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./Header.module.css";
+
+function Header() {
+  return (
+    <header>
+      <div className={styles.container}>Header</div>
+    </header>
+  );
+}
+
+export default Header;
